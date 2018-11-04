5bea970becd85cefbbadd0c15543ff2972bc7586
colin <colin.rhys.richards@gmail.com>
2018-11-03 21:53:37 -0600

    changes to the tiles

--- java.js
+++ java.js
@@ -1,14 +1,5 @@
-var coll = document.getElementsByClassName("collapsible");
-var i;
+var tiles = ["https://colinrhysrichards.github.io/Responsive-Portfolio/", "https://colinrhysrichards.github.io/Word-Guess-Game/"]
 
-for (i = 0; i < coll.length; i++) {
-    coll[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var content = this.nextElementSibling;
-        if (content.style.display === "block") {
-            content.style.display = "none";
-        } else {
-            content.style.display = "block";
-        }
-    });
-}
+console.log(tiles.length);
+console.log(tiles[0]);
+console.log(tiles[1]);
