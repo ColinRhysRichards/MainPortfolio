e92c604879813cc7ce669430932d2e5f0dbd1c43
colin <colin.rhys.richards@gmail.com>
2018-11-05 23:34:20 -0700

    pace section

--- java.js
+++ java.js
@@ -1,5 +1,11 @@
-var tiles = ["https://colinrhysrichards.github.io/Responsive-Portfolio/", "https://colinrhysrichards.github.io/Word-Guess-Game/"]
+// var tiles = ["https://colinrhysrichards.github.io/Responsive-Portfolio/", "https://colinrhysrichards.github.io/Word-Guess-Game/"]
 
-console.log(tiles.length);
-console.log(tiles[0]);
-console.log(tiles[1]);
+// console.log(tiles.length);
+// console.log(tiles[0]);
+// console.log(tiles[1]);
+
+$(document).ready(function (){
+    $(".btn").click(function(){
+$(window).scrollTop(10);
+})
+});
