6a1a9fceaae0734289124b373d948471c109ad29
colin <colin.rhys.richards@gmail.com>
2019-02-14 20:06:35 -0700

    style changes of the whole page. adding javascript

---
+++ portfolio.js
@@ -0,0 +1,7 @@
+    function mouseEnter() {
+        document.getElementsByClassName("practice").style.display = "inline-block";
+      }
+      
+      function mouseLeave() {
+        document.getElementsByClassName("practice").style.display = "none";
+      }
